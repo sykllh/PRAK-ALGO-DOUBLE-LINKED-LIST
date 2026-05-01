d7c886e6e73a7c7fa1e811c693dfb5b3b28214f6
sykllh <sykllhidmi@gmail.com>
2026-05-01 15:26:44 +0700

    newNode.next = START

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -36,5 +36,14 @@ public:
 
         // Step 3: Insert at beginning if list is empty or nim is smallest
         if (START == NULL || nim <= START->noMhs)
+        {
+            if(START != NULL && nim == START->noMhs)
+            {
+                cout << "\nDuplicate number not allowed" << endl;
+                return;
+            }
+            // Step 4: newNode.next = START
+            newNode->next = START;
+        }
     }
 }

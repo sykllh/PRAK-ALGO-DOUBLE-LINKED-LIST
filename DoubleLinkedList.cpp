f43365d10beeba50d20ba7ced4e507a03da0fae5
sykllh <sykllhidmi@gmail.com>
2026-05-01 15:30:03 +0700

    START = newNode

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -51,6 +51,10 @@ public:
 
             // Step 6: newNode.prev = NULL
             newNode->prev = NULL;
+
+            // Step 7: START = newNode
+            START = newNode;
+            return;
         }
     }
 }
